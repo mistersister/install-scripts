d3f10d2dc18463aee9e340a58fca7e2353483bca
mistersister <xarlieb@gmail.com>
2022-06-20 18:01:04 +0200

    removed nvidia driver
    Finally free of Team Green chains

--- post_install_fedora.sh
+++ post_install_fedora.sh
@@ -78,7 +78,7 @@ systemctl enable sddm
 systemcftl set-default graphical.target
 
 
-# nvidia driver from rpmfusion
+# add rpmfusion
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
